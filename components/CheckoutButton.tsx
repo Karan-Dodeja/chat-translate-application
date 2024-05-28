b94b164bab684aa4f06952419750051d779632f9
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-05-28 23:53:02 +0530

    Build Checkout Button Component

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import { db } from "@/firebase";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 const CheckoutButton = () => {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
   const createCHeckoutSession = async () => {
-    if (!session) return;
+    if (!session?.user.id) return;
+    setLoading(true);
+    const docRef = await addDoc(
+      collection(db, "customers", session.user.id, "checkout_sessions"),
+      {
+        price: "price_1O0998KDjTc6FlwiIL3eBoYQ",
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
+    return onSnapshot(docRef, (snap) => {
+      const data = snap.data();
+      const url = data?.url;
+      const error = data?.error;
+
+      if (error) {
+        alert(`An error occured: ${error.message}`);
+        setLoading(false);
+      }
+    });
   };
 
   return (
@@ -21,7 +44,7 @@ const CheckoutButton = () => {
     cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white 
     disabled:cursor-default"
       >
-        Sign Up
+        {loading ? "loading..." : "Sign up"}
       </button>
     </div>
   );
