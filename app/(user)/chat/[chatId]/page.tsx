5b0b2505657fdecd6217b2149a357abef5c9f84b
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-05-31 05:23:43 +0530

    Build chat messageing component

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,5 +1,6 @@
 import { authOptions } from "@/auth";
 import ChatInput from "@/components/ChatInput";
+import ChatMessages from "@/components/ChatMessages";
 import { getServerSession } from "next-auth";
 
 type Props = {
@@ -12,8 +13,14 @@ async function ChatPage({ params: { chatId } }: Props) {
   const session = await getServerSession(authOptions);
   return (
     <>
+      <div className="flex-1">
+        <ChatMessages
+          chatId={chatId}
+          session={session}
+          initialMessages={initialMessages}
+        />
+      </div>
       <ChatInput chatId={chatId} />
-      Chat
     </>
   );
 }
