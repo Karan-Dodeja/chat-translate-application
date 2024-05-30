a9b3502d246a73ceba65de3198392e43dd6168b2
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-05-31 02:21:49 +0530

    Added loading component

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,9 +1,9 @@
-function ChatPage() {
-  return {
-    <>
-    
-  </>
-};
+import { authOptions } from "@/auth";
+import { getServerSession } from "next-auth";
+
+async function ChatPage() {
+  const session = await getServerSession(authOptions);
+  return <>Chat</>;
 }
 
 export default ChatPage;
